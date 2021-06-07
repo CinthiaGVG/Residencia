449c5a3a8d81f05765521af953b2e2dc47a1d308
CinthiaBV <cinthia.vargas18@tectijuana.edu.mx>
2021-06-06 19:10:48 -0700

    commiting changes

--- karatera/core/views.py
+++ karatera/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 
     
 def home(request):
     return render(request,'core/home.html')
+    
 
 def conducta(request):
     return render(request,'core/conducta.html')
